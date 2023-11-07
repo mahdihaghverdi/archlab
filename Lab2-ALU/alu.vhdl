d5b5ba84f68597fc6c53c3408db7f690d03d8180
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2023-11-07 19:53:14 +0330

    Define the right middle-level signals

--- Lab2-ALU/alu.vhdl
+++ Lab2-ALU/alu.vhdl
@@ -5,32 +5,24 @@ use ieee.numeric_std.all;
 
 entity ALU is
     port (
-        A, B: in std_logic_vector(31 downto 0);
-        S   : out std_logic_vector(31 downto 0);
+        a, b: in std_logic_vector(31 downto 0);
+        s   : out std_logic_vector(31 downto 0);
         z, c, ovf: out std_logic;
         m   : in std_logic_vector (3 downto 0)
     );
 end ALU;
 
 architecture arch of ALU is
-    component mux8x1 is
-        port (
-            i0, i1, i2, i3,
-            i4, i5, i6, i7 : in std_logic_vector (7 downto 0);
-            sel            : in std_logic_vector (2 downto 0);
-            S         : out std_logic_vector (7 downto 0)
-        );
-    end component;
+    signal add, addu, sub, subu, slt, sltu, and, or, nor, xor: std_logic_vector(31 downto 0);
 
-    signal plus, minus, mul, mand, mor, mnor, mnand : std_logic_vector(7 downto 0);
 begin
-    plus  <= std_logic_vector(unsigned(A) + unsigned(B));
-    minus <= std_logic_vector(unsigned(A) - unsigned(B));
-    mul   <= std_logic_vector((unsigned(A(3 downto 0))) * (unsigned(B(3 downto 0))));
-    mand  <= A and B;
-    mor   <= A or B;
-    mnor  <= A nor B;
-    mnand <= A nand B;
+    plus  <= std_logic_vector(unsigned(a) + unsigned(b));
+    minus <= std_logic_vector(unsigned(a) - unsigned(b));
+    mul   <= std_logic_vector((unsigned(a(3 downto 0))) * (unsigned(b(3 downto 0))));
+    mand  <= a and b;
+    mor   <= a or b;
+    mnor  <= a nor b;
+    mnand <= a nand b;
 
     inner_mux : entity work.mux8x1 port map (
         i0 => "11111111",
@@ -42,6 +34,6 @@ begin
         i6 => mnor,
         i7 => mnand,
         sel => sel,
-        S => S
+        s => s
     );
 end arch;
