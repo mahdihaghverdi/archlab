0b1ba18eb6c0c09c5e118da73aaee5516ea50680
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2023-11-07 20:40:04 +0330

    write the basic add and sub without overflow or carry logic

--- Lab2-ALU/alu.vhdl
+++ Lab2-ALU/alu.vhdl
@@ -6,34 +6,25 @@ use ieee.numeric_std.all;
 entity ALU is
     port (
         a, b: in std_logic_vector(31 downto 0);
+        m   : in std_logic_vector (3 downto 0);
         s   : out std_logic_vector(31 downto 0);
-        z, c, ovf: out std_logic;
-        m   : in std_logic_vector (3 downto 0)
+        z, c, ovf: out std_logic
     );
 end ALU;
 
 architecture arch of ALU is
-    signal add, addu, sub, subu, slt, sltu, and, or, nor, xor: std_logic_vector(31 downto 0);
+    signal add, addu, sub, subu, slt, sltu, andx, orx, norx, xorx: std_logic_vector(31 downto 0);
 
 begin
-    plus  <= std_logic_vector(unsigned(a) + unsigned(b));
-    minus <= std_logic_vector(unsigned(a) - unsigned(b));
-    mul   <= std_logic_vector((unsigned(a(3 downto 0))) * (unsigned(b(3 downto 0))));
-    mand  <= a and b;
-    mor   <= a or b;
-    mnor  <= a nor b;
-    mnand <= a nand b;
-
-    inner_mux : entity work.mux8x1 port map (
-        i0 => "11111111",
-        i1 => plus,
-        i2 => minus,
-        i3 => mul,
-        i4 => mand,
-        i5 => mor,
-        i6 => mnor,
-        i7 => mnand,
-        sel => sel,
-        s => s
-    );
+process(m, a, b) is
+begin
+    case m is
+        when "0000" =>
+            s <= std_logic_vector(signed(a) + signed(b));
+        when "0010" =>
+            s <= std_logic_vector(signed(a) - signed(b));
+        when others => -- 'U', 'X', '-', etc.
+            s <= "11111111111111111111111111111111";
+    end case;
+end process;
 end arch;
