027b737a4c103e330d890e48d67f996e4022bc5b
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2023-11-07 21:28:50 +0330

    slt is now working

--- Lab2-ALU/alu.vhdl
+++ Lab2-ALU/alu.vhdl
@@ -13,17 +13,32 @@ entity ALU is
 end ALU;
 
 architecture arch of ALU is
-    signal add, addu, sub, subu, slt, sltu, andx, orx, norx, xorx: std_logic_vector(31 downto 0);
-
+    signal temp, add, addu, sub, subu, slt, sltu, andx, orx, norx, xorx: std_logic_vector(31 downto 0);
 begin
 process(m, a, b) is
+constant zeros: std_logic_vector := "00000000000000000000000000000000";
 begin
     case m is
-        when "0000" =>
+        when "0000" =>  -- add
             s <= std_logic_vector(signed(a) + signed(b));
-        when "0010" =>
+        when "0001" =>  -- addu
+            s <= std_logic_vector(unsigned(a) + unsigned(b));
+        when "0010" =>  -- sub
             s <= std_logic_vector(signed(a) - signed(b));
-        when others => -- 'U', 'X', '-', etc.
+        when "0011" =>  -- subu
+            s <= std_logic_vector(unsigned(a) - unsigned(b));
+        when "0100" =>  -- slt
+            if signed(a) < signed(b) then
+                s <= (0 => '1', others => '0');
+            else
+                s <= (others => '0');
+            end if;
+        when "0101" =>  -- sltu
+        when "0110" =>  -- and
+        when "0111" =>  -- or
+        when "1000" =>  -- nor
+        when "1001" =>  -- xor
+        when others =>
             s <= (others => 'U');
     end case;
 end process;
