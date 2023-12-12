3b33be8d28306ea57a9a359fb9ce08b897f0f530
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2023-12-12 23:36:53 +0330

    add the input changes to doc

--- Lab3-Memory/Lab3Files/romram.vhd
+++ Lab3-Memory/Lab3Files/romram.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 entity romram is
    port(
       clk_100MHZ: in std_logic;
-      switch: in std_logic;
+      switch0, switch1: in std_logic;
       leds: out std_logic_vector(7 downto 0)
 );
 end romram;
@@ -39,14 +39,15 @@ architecture structural of romram is
    signal addr_counter: std_logic_vector(2 downto 0) := "000";
    signal dout_rom,dout_bram: std_logic_vector(7 downto 0);
    signal dina_null: std_logic_vector(7 downto 0) := "00000000";
-   signal wea_null: std_logic_vector(0 downto 0) := "0";
+   signal before_leds: std_logic_vector(7 downto 0);
+   signal all_null: std_logic_vector(7 downto 0) := "00000000";
 
 begin
    clock_divider: clock_divide port map(clk_100MHz, clk_1Hz);
    rom: rom8x8 port map(addr_counter, dout_rom);
    bram: bram8x8 port map(
       clka => clk_1Hz,
-      wea => wea_null,
+      wea => switch1,
       addra => addr_counter,
       dina => dina_null,
       douta => dout_bram
@@ -55,14 +56,25 @@ begin
    process(clk_1Hz) is
    begin
       if(clk_1Hz'event and clk_1Hz = '1') then
-         case switch is
+         case switch0 is
             when '0' =>
-               leds <= dout_rom;
+               before_leds <= dout_rom;
             when '1' =>
-               leds <= dout_bram;
+               before_leds <= dout_bram;
             when others => null;
          end case;
          addr_counter <= std_logic_vector(unsigned(addr_counter) + 1);
       end if;
+   end process;
+
+   process(before_leds)
+   begin
+      case switch1 is
+         when '0' =>
+             leds <= before_leds;
+         when '1' =>
+             leds <= null;
+         when others => null;
+      end case;
    end process;
 end structural;
